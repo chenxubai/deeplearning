440851c1b1b968abb23efa582f72fa2489404a7c
chenxubai <285698489@qq.com>
2018-01-15 14:26:45 +0800

    一个小的阶段，基于《机器学习》的脸部图片，四个方向，10个人，训练三个方向，测试1个方向，识别率达到  numEpochs=24   # of classes:    10  Accuracy:        0.9114  Precision:       0.9394  Recall:          0.9125  F1 Score:        0.8864  numEpochs>24 #一直到100都不再有变化  Accuracy:        0.9114  Precision:       0.9394  Recall:          0.9125  F1 Score:        0.8864

---
+++ fr/src/main/java/org/deeplearning4j/examples/dataexamples/ImagePipelineExample.java
@@ -0,0 +1,168 @@
+package org.deeplearning4j.examples.dataexamples;
+
+import java.io.File;
+import java.util.Random;
+
+import org.datavec.api.io.filters.BalancedPathFilter;
+import org.datavec.api.io.labels.ParentPathLabelGenerator;
+import org.datavec.api.split.FileSplit;
+import org.datavec.api.split.InputSplit;
+import org.datavec.api.util.ClassPathResource;
+import org.datavec.image.loader.BaseImageLoader;
+import org.datavec.image.recordreader.ImageRecordReader;
+import org.datavec.image.transform.ImageTransform;
+import org.datavec.image.transform.MultiImageTransform;
+import org.datavec.image.transform.ShowImageTransform;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by susaneraly on 6/9/16.
+ */
+public class ImagePipelineExample {
+
+    //Images are of format given by allowedExtension -
+    private static final String [] allowedExtensions = BaseImageLoader.ALLOWED_FORMATS;
+
+    private static final long seed = 12345;
+
+    private static final Random randNumGen = new Random(seed);
+
+    private static final int height = 50;
+    private static final int width = 50;
+    private static final int channels = 3;
+    private static Logger log = LoggerFactory.getLogger(ImagePipelineExample.class);
+    public static void main(String[] args) throws Exception {
+
+        //DIRECTORY STRUCTURE:
+        //Images in the dataset have to be organized in directories by class/label.
+        //In this example there are ten images in three classes
+        //Here is the directory structure
+        //                                    parentDir
+        //                                  /    |     \
+        //                                 /     |      \
+        //                            labelA  labelB   labelC
+        //
+        //Set your data up like this so that labels from each label/class live in their own directory
+        //And these label/class directories live together in the parent directory
+        //
+        //
+        File parentDir=new ClassPathResource("DataExamples/ImagePipeline/").getFile();
+        //Files in directories under the parent dir that have "allowed extensions" split needs a random number generator for reproducibility when splitting the files into train and test
+        FileSplit filesInDir = new FileSplit(parentDir, allowedExtensions, randNumGen);
+
+        //You do not have to manually specify labels. This class (instantiated as below) will
+        //parse the parent dir and use the name of the subdirectories as label/class names
+        ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
+        //The balanced path filter gives you fine tune control of the min/max cases to load for each class
+        //Below is a bare bones version. Refer to javadoc for details
+        BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelMaker);
+
+        //Split the image files into train and test. Specify the train test split as 80%,20%
+        InputSplit[] filesInDirSplit = filesInDir.sample(pathFilter, 80, 20);
+        InputSplit trainData = filesInDirSplit[0];
+        //InputSplit testData = filesInDirSplit[1];  //The testData is never used in the example, commenting out.
+
+        //Specifying a new record reader with the height and width you want the images to be resized to.
+        //Note that the images in this example are all of different size
+        //They will all be resized to the height and width specified below
+        ImageRecordReader recordReader = new ImageRecordReader(height,width,channels,labelMaker);
+
+        //Often there is a need to transforming images to artificially increase the size of the dataset
+        //DataVec has built in powerful features from OpenCV
+        //You can chain transformations as shown below, write your own classes that will say detect a face and crop to size
+        /*ImageTransform transform = new MultiImageTransform(randNumGen,
+            new CropImageTransform(10), new FlipImageTransform(),
+            new ScaleImageTransform(10), new WarpImageTransform(10));
+            */
+
+        //You can use the ShowImageTransform to view your images
+        //Code below gives you a look before and after, for a side by side comparison
+        ImageTransform transform = new MultiImageTransform(randNumGen,new ShowImageTransform("Display - before "));
+
+        //Initialize the record reader with the train data and the transform chain
+        recordReader.initialize(trainData,transform);
+        int outputNum = recordReader.numLabels();
+        //convert the record reader to an iterator for training - Refer to other examples for how to use an iterator
+        int batchSize = 10; // Minibatch size. Here: The number of images to fetch for each call to dataIter.next().
+        int labelIndex = 1; // Index of the label Writable (usually an IntWritable), as obtained by recordReader.next()
+        // List<Writable> lw = recordReader.next();
+        // then lw[0] =  NDArray shaped [1,3,50,50] (1, heightm width, channels)
+        //      lw[0] =  label as integer.
+
+        DataSetIterator dataIter = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex, outputNum);
+        
+        //Get the DataSetIterators:
+        DataSetIterator mnistTrain = dataIter;
+        DataSetIterator mnistTest = dataIter;        
+
+        final int numRows = 30;
+        final int numColumns = 32;
+
+        int rngSeed = 123; // random number seed for reproducibility
+        int numEpochs = 15; // number of epochs to perform
+        double rate = 0.0015; // learning rate
+
+        log.info("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+            .seed(rngSeed) //include a random seed for reproducibility
+            .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT) // use stochastic gradient descent as an optimization algorithm
+            .iterations(1)
+            .activation(Activation.RELU)
+            .weightInit(WeightInit.XAVIER)
+            .learningRate(rate) //specify the learning rate
+            .updater(new Nesterovs(0.98))
+            .regularization(true).l2(rate * 0.005) // regularize learning model
+            .list()
+            .layer(0, new DenseLayer.Builder() //create the first input layer.
+                    .nIn(numRows * numColumns)
+                    .nOut(500)
+                    .build())
+            .layer(1, new DenseLayer.Builder() //create the second input layer
+                    .nIn(500)
+                    .nOut(100)
+                    .build())
+            .layer(2, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD) //create hidden layer
+                    .activation(Activation.SOFTMAX)
+                    .nIn(100)
+                    .nOut(outputNum)
+                    .build())
+            .pretrain(false).backprop(true) //use backpropagation to adjust weights
+            .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(new ScoreIterationListener(5));  //print the score with every iteration
+
+        log.info("Train model....");
+        for( int i=0; i<numEpochs; i++ ){
+        	log.info("Epoch " + i);
+            model.fit(mnistTrain);
+        }
+
+        
+        while (dataIter.hasNext()) {
+            DataSet ds = dataIter.next();
+            System.out.println(ds);
+            try {
+                Thread.sleep(3000);                 //1000 milliseconds is one second.
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
